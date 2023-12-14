e81e266daea727b70f6ad254036ce30d409e4d23
surya <suryaprakasam@innoboon.com>
2023-12-14 12:06:37 +0530

    Inintial commit for the playground app

---
+++ base_app/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Base app for the dev practise',
+  description: 'Playground for the react',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ base_app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+  <>
+  <div className='flex min-h-screen justify-center items-center'>
+    This is the main screen of  the  next practise application 
+  </div>
+  </>
+  )
+}
